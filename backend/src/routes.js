cd7d1ad4ff4c5e3d54fad423ab7d8f35414f7340
Junior Alves <jjalves96@gmail.com>
2019-12-03 05:46:30 -0300

    Conection mongoDB

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,15 @@
 import { Router } from 'express';
+import mongoose from 'mongoose';
 
 const routes = new Router();
+
+mongoose.connect(
+  'mongodb+srv://semana:semana@cluster0-0laro.mongodb.net/barber?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 routes.get('/', (req, res) => {
   return res.json({ ok: 'true' });
